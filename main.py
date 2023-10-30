0d7c2caa76f992ca55ddc479ce8fa3a9545a6daa
Aleksandr <alexandr.bogachev@gmail.com>
2023-10-30 20:07:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# print("Hello world!")
+
+# print(4, 8, 15, 16, 23, 42)
+
+# print('*')
+# print('**')
+# print('***')
+# print('****')
+# print('*****')
+
+# st = input('Введите Ваше имя ')
+# print('Привет,', st + '!')
+
+# st = input()
+# print(st, "- чемпион!")
+
+# print('I', 'like', 'Python', sep='***', end='!')
+# print('I', 'like', 'Python', sep='***')
+
+# st, string1, string2, string3 = input(), input(), input(), input()
+# print(string1, string2, string3, sep=st)
+# print(string1, end=st)
+# print(string2, end=st)
+# print(string3, end=st)
+
+# num = int(input())
+# print(num, num + 1, num + 2)
+
+# a, b, c = int(input()), int(input()), int(input())
+# print('Сумма трех чисел:', a, b, c, 'равна', a + b + c)
+
+# a, b = int(input()), int(input())
+# print(a, '+', b, '=', a + b)
+# print(a, '-', b, '=', a - b)
+# print(a, '*', b, '=', a * b)
+# c = a / b
+# print(a, '/', b, '=', c)
+
+# +, -, *, /
+# % - нахождение остатка от деления, // - целочисленное деление
+# print(17 % 10)
+# print(17 // 10)
+
+a = int(input())
+V = a ** 3
+S = 6 * (a ** 2)
+print('Объем куба равен:', V)
+print("Площадь полной поверхности куба:", S)
+
+
+
